a5532571ecd0eb90c904bf09d2860cab83d4cfe4
Cody Worthen <cworthen777@hotmail.com>
2022-01-20 16:59:05 -0700

    implemented DoublyLinkedList

--- linked_lists/DoublyLinkedList.java
+++ linked_lists/DoublyLinkedList.java
@@ -1,5 +1,103 @@
 package linked_lists;
 
-public class DoublyLinkedList {
+public class DoublyLinkedList<E> {
+
+	private static class Node<E> {
+		private E element;
+		private Node<E> prev;
+		private Node<E> next;
+
+		public Node(E element, Node<E> prev, Node<E> next) {
+			this.element = element;
+			this.prev = prev;
+			this.next = next;
+		}
+
+		public E getElement() {
+			return element;
+		}
+
+		public Node<E> getPrev() {
+			return prev;
+		}
+
+		public void setPrev(Node<E> prev) {
+			this.prev = prev;
+		}
+
+		public Node<E> getNext() {
+			return next;
+		}
+
+		public void setNext(Node<E> next) {
+			this.next = next;
+		}
+	}
+
+	private Node<E> header = null;
+	private Node<E> trailer = null;
+	private int size = 0;
+
+	public DoublyLinkedList() {
+		header = new Node<>(null, null, null);
+		trailer = new Node<>(null, header, null);
+		header.setNext(trailer);
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public E first() {
+		if (isEmpty())
+			return null;
+		return header.getNext().getElement();
+	}
+
+	public E last() {
+		if (isEmpty())
+			return null;
+		return trailer.getPrev().getElement();
+	}
+
+	public void addFirst(E element) {
+		addBetween(element, header, header.getNext());
+	}
+
+	public void addLast(E element) {
+		addBetween(element, trailer.getPrev(), trailer);
+	}
+
+	public E removeFirst() {
+		if (isEmpty())
+			return null;
+		return remove(header.getNext());
+	}
+
+	public E removeLast() {
+		if (isEmpty())
+			return null;
+		return remove(trailer.getPrev());
+	}
+
+	public void addBetween(E element, Node<E> predecessor, Node<E> successor) {
+		Node<E> newest = new Node<>(element, predecessor, successor);
+		predecessor.setNext(newest);
+		successor.setPrev(newest);
+		size++;
+	}
+
+	public E remove(Node<E> node) {
+		Node<E> predecessor = node.getPrev();
+		Node<E> successor = node.getNext();
+		predecessor.setNext(successor);
+		successor.setPrev(predecessor);
+		size--;
+		return node.getElement();
+	}
 
 }
