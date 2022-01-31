3799dee5271d3c4a5cfdbcdbf4a387f5f1b327f9
Cody Worthen <cworthen777@hotmail.com>
2022-01-31 15:06:17 -0700

    fixed javadoc comments

--- stacks/ArrayStack.java
+++ stacks/ArrayStack.java
@@ -12,37 +12,22 @@ public class ArrayStack<E> implements Stack<E> {
 	private E[] data;
 	private int t = -1;
 
-	/**
-	 * Default constructor
-	 */
 	public ArrayStack() {
 		this(CAPACITY);
 	}
 
-	/**
-	 * @param capacity the maximum allowed size of the stack (immutable)
-	 */
 	public ArrayStack(int capacity) {
 		data = (E[]) new Object[capacity];
 	}
 
-	/**
-	 * @return int
-	 */
 	public int size() {
 		return (t + 1);
 	}
 
-	/**
-	 * @return boolean
-	 */
 	public boolean isEmpty() {
 		return t == -1;
 	}
 
-	/**
-	 * @return void
-	 */
 	public void push(E element) {
 		if (size() == data.length) {
 			throw new IllegalStateException("Stack is full");
@@ -50,9 +35,6 @@ public class ArrayStack<E> implements Stack<E> {
 		data[++t] = element; // increments t before the expression is evaluated
 	}
 
-	/**
-	 * @return E
-	 */
 	public E top() {
 		if (isEmpty()) {
 			return null;
@@ -60,9 +42,6 @@ public class ArrayStack<E> implements Stack<E> {
 		return data[t];
 	}
 
-	/**
-	 * @return E
-	 */
 	public E pop() {
 		if (isEmpty()) {
 			return null;
