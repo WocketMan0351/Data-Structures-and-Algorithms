92d0d00e1ad23fb72c70c08b6cbe6c9563832c84
Cody Worthen <cworthen777@hotmail.com>
2022-03-23 16:37:58 -0700

    implemented ChainHashMap

--- hash_tables/ChainHashMap.java
+++ hash_tables/ChainHashMap.java
@@ -1,6 +1,9 @@
 package hash_tables;
 
+import java.util.ArrayList;
+
 import maps.UnsortedTableMap;
+import priority_queues.Entry;
 
 public class ChainHashMap<K, V> extends AbstractHashMap<K, V> {
 	// a fixed capacity array of unsortedTableMap that serve as buckets
@@ -23,6 +26,61 @@ public class ChainHashMap<K, V> extends AbstractHashMap<K, V> {
 	 */
 	protected void createTable() {
 		table = (UnsortedTableMap<K, V>[]) new UnsortedTableMap[capacity];
+	}
+
+	/**
+	 * Returns value associated with key k in bucket with hash value h, or else
+	 * null.
+	 */
+	protected V bucketGet(int h, K k) {
+		UnsortedTableMap<K, V> bucket = table[h];
+		if (bucket == null) {
+			return null;
+		}
+		return bucket.get(k);
+	}
+
+	/**
+	 * Associates key k with value v in bucket with hash value h.
+	 */
+	protected V bucketPut(int h, K k, V v) {
+		UnsortedTableMap<K, V> bucket = table[h];
+		if (bucket == null) {
+			bucket = table[h] = new UnsortedTableMap<>();
+		}
+		int oldSize = bucket.size();
+		V bucketToAdd = bucket.put(k, v);
+		n += (bucket.size() - oldSize); // size may have increased
+		return bucketToAdd;
+	}
+
+	/**
+	 * Removes entry having key key from bucket with hash value h (if any).
+	 */
+	protected V bucketRemove(int h, K k) {
+		UnsortedTableMap<K, V> bucket = table[h];
+		if (bucket == null) {
+			return null;
+		}
+		int oldSize = bucket.size();
+		V bucketToRemove = bucket.remove(k);
+		n -= (bucket.size() - oldSize); // size may have decreased
+		return bucketToRemove;
+	}
+
+	/**
+	 * Returns an iterable colelction of all key-value entries of the map.
+	 */
+	public Iterable<Entry<K, V>> entrySet() {
+		ArrayList<Entry<K, V>> buffer = new ArrayList<>();
+		for (int h = 0; h < capacity; h++) {
+			if (table[h] != null) {
+				for (Entry<K, V> entry : table[h].entrySet()) {
+					buffer.add(entry);
+				}
+			}
+		}
+		return buffer;
 	}
 
 }
