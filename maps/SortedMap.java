beffe3db889ea64637f220a1fd162ce00503a0f3
Cody Worthen <cworthen777@hotmail.com>
2022-03-25 16:48:15 -0700

    working on binary search trees

---
+++ maps/SortedMap.java
@@ -0,0 +1,60 @@
+package maps;
+
+import priority_queues.Entry;
+
+public interface SortedMap<K, V> extends Map<K, V> {
+
+	/**
+	 * Returns the entry with the smallest key value (or null, if the map is empty).
+	 * 
+	 * @return Entry
+	 */
+	Entry<K, V> firstEntry();
+
+	/**
+	 * Returns the entry with the largest key value (or null, if the map is empty).
+	 * 
+	 * @return Entry
+	 */
+	Entry<K, V> lastEntry();
+
+	/**
+	 * Returns the entry with the least key value greater than or equal to k (or
+	 * null, if no such entry exists).
+	 * 
+	 * @return Entry
+	 */
+	Entry<K, V> ceilingEntry(K k);
+
+	/**
+	 * Returns the entry with the greatest key value less than or equal to k (or
+	 * null, if no such entry exists).
+	 * 
+	 * @return Entry
+	 */
+	Entry<K, V> floorEntry(K k);
+
+	/**
+	 * Returns the entry with the greatest value strictly less than k (or null, if
+	 * no such entry exists).
+	 * 
+	 * @return Entry
+	 */
+	Entry<K, V> lowerEntry(K k);
+
+	/**
+	 * Returns the entry with the least key value strictly greater than k (or null,
+	 * if no such entry exists).
+	 * 
+	 * @return Entry
+	 */
+	Entry<K, V> higherEntry(K k);
+
+	/**
+	 * Returns an iteration of all entries with key greater tahn or equal to k1, but
+	 * strictly less than k2.
+	 * 
+	 * @return Entry
+	 */
+	Iterable<Entry<K, V>> subMap(K k1, K k2);
+}
